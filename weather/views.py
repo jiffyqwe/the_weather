f794f14cb2ca1af0ebe1eca006900b7da8bfe651
jiffyqwe <jiffyqwe123@gmail.com>
2022-04-20 11:35:07 +0300

    json

--- weather/views.py
+++ weather/views.py
@@ -36,8 +36,8 @@ def verify(request):
     print("data : " + json.dumps(data))
     req = requests.post(url, data=json.dumps(data)).json()
     print(json.dumps(req))
-    if req.code == 200 :
-        return req.result
+    if req['code'] == 200 :
+        return req['result']
     return False
 
 
